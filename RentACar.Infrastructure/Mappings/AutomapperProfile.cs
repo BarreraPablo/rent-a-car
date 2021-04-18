bc2d12030da7aa618c87a4d6650023533ba587ab
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-04-18 11:26:44 -0300

    Configure automapper

---
+++ RentACar.Infrastructure/Mappings/AutomapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RentACar.Core.DTOs.BrandDTOs;
+using RentACar.Core.Entities;
+
+namespace RentACar.Infrastructure.Mappings
+{
+    class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            //CreateMap<BrandCreateDto, Brand>();
+        }
+    }
+}
