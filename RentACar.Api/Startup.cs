64a24190eddf19164fc4a8c8f1b435405d46f1e2
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-04-25 11:56:13 -0300

    Add password hashing service

--- RentACar.Api/Startup.cs
+++ RentACar.Api/Startup.cs
@@ -14,7 +14,10 @@ using RentACar.Core.Interfaces;
 using RentACar.Core.Services;
 using RentACar.Infrastructure.Data;
 using RentACar.Infrastructure.Filters;
+using RentACar.Infrastructure.Interfaces;
+using RentACar.Infrastructure.Options;
 using RentACar.Infrastructure.Repositories;
+using RentACar.Infrastructure.Services;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -57,12 +60,15 @@ namespace RentACar.Api
                 c.IncludeXmlComments(xmlPath);
             });
 
+            services.Configure<PasswordOptions>(Configuration.GetSection("PasswordOptions"));
+
             services.AddDbContext<RentACarContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("RentACar"))
             );
 
             services.AddScoped<IUserService, UserService>();
             services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddSingleton<IPasswordService, PasswordService>();
 
             services.AddAuthentication(options =>
             {
---
+++ RentACar.Infrastructure/Services/PasswordService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Options;
+using RentACar.Infrastructure.Interfaces;
+using RentACar.Infrastructure.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Infrastructure.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        private readonly PasswordOptions options;
+        public PasswordService(IOptions<PasswordOptions> options)
+        {
+            this.options = options.Value;
+        }
+        public bool Check(string hash, string password)
+        {
+            var parts = hash.Split('.', 3);
+
+            if (parts.Length != 3) {
+                throw new FormatException("Unexpected hash format");
+            }
+
+            var iterations = Convert.ToInt32(parts[0]);
+            var salt = Convert.FromBase64String(parts[1]);
+            var key = Convert.FromBase64String(parts[2]);
+
+            using (var algorithm = new Rfc2898DeriveBytes(
+               password,
+               salt,
+               iterations,
+               HashAlgorithmName.SHA512
+               ))
+            {
+                var keyToCheck = algorithm.GetBytes(options.KeySize);
+                
+                return keyToCheck.SequenceEqual(key);
+            }
+        }
+
+        public string Hash(string password)
+        {
+            using(var algorithm = new Rfc2898DeriveBytes(
+                password, 
+                options.SaltSize,
+                options.Iterations,
+                HashAlgorithmName.SHA512
+                ))
+            {
+                var key = Convert.ToBase64String(algorithm.GetBytes(options.KeySize));
+                var salt = Convert.ToBase64String(algorithm.Salt);
+
+                return $"{options.Iterations}.{salt}.{key}";
+            }
+        }
+    }
+}
