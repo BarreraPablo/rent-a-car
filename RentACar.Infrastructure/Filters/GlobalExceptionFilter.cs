5b6dcc8029be797bffe3faf241e44fbd41df7585
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-04-27 11:32:36 -0300

    Update global exception filter

--- RentACar.Infrastructure/Filters/GlobalExceptionFilter.cs
+++ RentACar.Infrastructure/Filters/GlobalExceptionFilter.cs
@@ -19,17 +19,11 @@ namespace RentACar.Infrastructure.Filters
                 var exception = (BussinessException)context.Exception;
                 var validation = new
                 {
-                    Status = 400,
-                    Title = "Bad Request",
-                    Detail = exception.Message
+                    BussinessValidation = new string[1] {exception.Message}
                 };
 
-                var json = new
-                {
-                    errors = new[] { validation }
-                };
 
-                context.Result = new BadRequestObjectResult(json);
+                context.Result = new BadRequestObjectResult(validation);
                 context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                 context.ExceptionHandled = true;
             }
