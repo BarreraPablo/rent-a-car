4a2613b756b53cf76a948c7c7a95f7c8d56037bd
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-05-08 13:55:18 -0300

    Adds bearer token option in swagger

--- RentACar.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ RentACar.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -62,6 +62,30 @@ namespace RentACar.Infrastructure.Extensions
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RentACar.Api", Version = "v1" });
 
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFileName);
                 c.IncludeXmlComments(xmlPath);
             });
