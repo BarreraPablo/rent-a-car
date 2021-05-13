2d388147210394bc6429be6b2c2164fe94b653cb
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-05-13 12:21:22 -0300

    Adds cars page

---
+++ RentACar.Presentation/src/Pages/Cars/Cars.js
@@ -0,0 +1,93 @@
+import { EditOutlined, EyeOutlined } from "@ant-design/icons";
+import { Button, Card, Space, Table, Tooltip } from "antd";
+import React, { useEffect, useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import * as carService from "../../services/carService";
+import "./Cars.css";
+
+const SHOW = "show";
+const EDIT = "edit";
+
+function Cars() {
+    const [tableData, setTableData] = useState([]);
+    let history = useHistory();
+
+    const columns = [
+        {
+            title: "Model",
+            dataIndex: "model",
+            key: "model",
+        },
+        {
+            title: "Brand",
+            dataIndex: "brand",
+            key: "brand",
+            render: (brand) => brand.name,
+        },
+        {
+            title: "Year",
+            dataIndex: "year",
+            key: "year",
+        },
+        {
+            title: "Body Type",
+            key: "bodyType",
+            dataIndex: "bodyType",
+            render: (bodyType) => bodyType.name,
+        },
+        {
+            title: "Seats",
+            dataIndex: "seats",
+            key: "seats",
+        },
+        {
+            title: "Price Per Day",
+            dataIndex: "pricePerDay",
+            key: "pricePerDay",
+        },
+        {
+            title: "Action",
+            key: "action",
+            render: (text, record) => (
+                <Space size="middle">
+                    <Tooltip title="Show">
+                        <Button
+                            shape="circle"
+                            onClick={() => redirectTo(SHOW, record.id)}
+                            icon={<EyeOutlined />}
+                        />
+                    </Tooltip>
+                    <Tooltip title="Edit">
+                        <Button
+                            shape="circle"
+                            onClick={() => redirectTo(EDIT, record.id)}
+                            icon={<EditOutlined />}
+                        />
+                    </Tooltip>
+                </Space>
+            ),
+        },
+    ];
+
+    const redirectTo = (action, id) => {
+        history.replace(`/cars/${action}/${id}`);
+    };
+
+    useEffect(() => {
+        carService.getCars().then((info) => setTableData(info));
+    }, []);
+
+    return (
+        <div className="site-card-border-less-wrapper">
+            <Card title="Cars managment" bordered={false}>
+                <NavLink to="/cars/new">
+                    <Button type="primary">Create new</Button> <br />
+                    <br />
+                </NavLink>
+                <Table columns={columns} rowKey="id" dataSource={tableData} />
+            </Card>
+        </div>
+    );
+}
+
+export default Cars;
---
+++ RentACar.Presentation/src/services/carService.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const api_url = process.env.REACT_APP_API_URL;
+
+export const getCars = () => {
+    return axios
+        .get(api_url + "car")
+        .then((res) => {
+            return Promise.resolve(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+export const getById = (id) => {
+    return axios
+        .get(api_url + `car/${id}`)
+        .then((res) => {
+            return Promise.resolve(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+export const saveCar = (car, action) => {
+    console.log("desde cars service, car", car);
+    const formdata = new FormData();
+
+    formdata.append("id", car.id);
+    formdata.append("model", car.model);
+    formdata.append("brandId", car.brandId);
+    formdata.append("doors", car.doors);
+    formdata.append("airConditioner", car.airConditioner);
+    formdata.append("pricePerDay", car.pricePerDay);
+    formdata.append("gearbox", car.gearbox);
+    formdata.append("year", car.year);
+    formdata.append("bodyTypeId", car.bodyTypeId);
+    formdata.append("seats", car.seats);
+    formdata.append("available", car.available);
+    formdata.append("image", car.image);
+
+    console.log('cars service, car:', car, 'action', action);
+    return axios({
+        method: action === "new" ? "post" : "put",
+        url: api_url + "car",
+        data: formdata,
+    })
+        .then(() => Promise.resolve())
+        .catch((err) => Promise.reject("Something gone wrong saving the car.", err));
+};
