e8682f7a4a754ce1d662022a4678e5517eec89e0
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-05-19 08:53:07 -0300

    Adds body type service

--- RentACar.Presentation/src/services/bodyTypeService.js
+++ RentACar.Presentation/src/services/bodyTypeService.js
@@ -8,3 +8,23 @@ export const getAll = () => {
         .then((res) => Promise.resolve(res.data))
         .catch(err => Promise.reject('Something gone wrong getting the body types'));
 };
+
+export const save = (bodyType, action) => {
+
+
+    return axios({
+        method: action === "new" ? "post" : "put",
+        url: api_url + "bodytype",
+        data: bodyType,
+    })
+        .catch((err) => {
+            if (err.response && err.response.status === 400) {
+                if(err.response.data && err.response.data.name){
+                    return Promise.reject(err.response.data.name[0]);
+                }
+
+                return Promise.reject("Please verify the information submitted.");
+            }
+            return Promise.reject("Something gone wrong saving the reservation.");
+        });
+}
