2648b054544329c636068341e029ef775018c66e
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-05-19 09:35:49 -0300

    Updates paymentTypeService

--- RentACar.Presentation/src/services/paymentTypeService.js
+++ RentACar.Presentation/src/services/paymentTypeService.js
@@ -10,3 +10,23 @@ export const getAll = () => {
             Promise.reject("Something gone wrong getting the payments types")
         );
 };
+
+export const save = (paymentType, action) => {
+
+
+    return axios({
+        method: action === "new" ? "post" : "put",
+        url: api_url + "paymentType",
+        data: paymentType,
+    })
+        .catch((err) => {
+            if (err.response && err.response.status === 400) {
+                if(err.response.data && err.response.data.name){
+                    return Promise.reject(err.response.data.name[0]);
+                }
+
+                return Promise.reject("Please verify the information submitted.");
+            }
+            return Promise.reject("Something gone wrong saving the reservation.");
+        });
+}
