1b29a4d795876a57a17ea53d05dbbddd61ca5a41
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-05-19 09:22:44 -0300

    Adds save method in brandService

--- RentACar.Presentation/src/services/brandService.js
+++ RentACar.Presentation/src/services/brandService.js
@@ -8,3 +8,25 @@ export const getAll = () => {
         return Promise.resolve(res.data);
     }).catch(err => Promise.reject('Something gone wrong getting the brands.'));
 }
+
+
+export const save = (brand, action) => {
+
+
+    return axios({
+        method: action === "new" ? "post" : "put",
+        url: api_url + "brand",
+        data: brand,
+    })
+        .catch((err) => {
+            if (err.response && err.response.status === 400) {
+                if(err.response.data && err.response.data.name){
+                    return Promise.reject(err.response.data.name[0]);
+                }
+
+                return Promise.reject("Please verify the information submitted.");
+            }
+            return Promise.reject("Something gone wrong saving the reservation.");
+        });
+}
+
