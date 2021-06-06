e97daf59e06c9ad76b0ae8f65a8ddf5bf94ccfb6
Pablo Barrera <pablomartinbarrera@hotmail.com>
2021-06-06 17:18:04 -0300

    Add password recovery page

--- RentACar.Presentation/src/App.js
+++ RentACar.Presentation/src/App.js
@@ -19,6 +19,7 @@ import Brand from "./Pages/Brands/Brand";
 import PaymentType from "./Pages/PaymentTypes/PaymentType";
 import DocumentType from "./Pages/DocumentTypes/DocumentType";
 import RegisterForm from "./Pages/Register/Register";
+import PasswordRecovery from "./Pages/PasswordRecovery/PasswordRecovery";
 
 function App() {
     return (
@@ -39,6 +40,7 @@ function App() {
                     </MainLayout>
                 </PrivateRoute>
                 <Route exact path="/register" component={RegisterForm} />
+                <Route exact path="/passwordrecovery" component={PasswordRecovery} />
                 <Route exact path={["/", "/login"]} component={Login} />
             </Router>
         </ProvideAuth>
